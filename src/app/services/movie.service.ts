ad9e5539708ba47a35ca51dfe42e097dc938253e
Shameemudheen M P <shameem.mpmanalaya@gmail.com>
2025-09-28 22:31:01 +0530

    build config changes

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -2,20 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
-
-  constructor(private httpclient: HttpClient) { }
+  constructor(private httpclient: HttpClient) {}
   getTrendingMovies() {
-    return this.httpclient.get('../../assets/data/trending-movies.json');
+    return this.httpclient.get('assets/data/trending-movies.json');
   }
 
   getTheatreMovies() {
-    return this.httpclient.get('../../assets/data/theatre-movies.json');
+    return this.httpclient.get('assets/data/theatre-movies.json');
   }
-  
+
   getPoplularMovies() {
-    return this.httpclient.get('../../assets/data/popular-movies.json');
+    return this.httpclient.get('assets/data/popular-movies.json');
   }
 }
